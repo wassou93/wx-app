0d4ed90f32ccd62df1b5d5b1193cecd11e25675a
Wassim Akkari <was.akkari@gmail.com>
2026-04-26 12:49:42 +0100

    set size and center the app when first spawned.

--- src/App.cpp
+++ src/App.cpp
@@ -5,6 +5,8 @@ wxIMPLEMENT_APP(MyApp);
 
 bool MyApp::OnInit() {
     MyFrame* frame = new MyFrame();
+    frame->SetClientSize(800, 400);
+    frame->Center();
     frame->Show(true);
     return true;
 }
